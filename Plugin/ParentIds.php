3fb1b8b11ec0de96cc8866f6a55beb925e141160
Wojciech M. Wnuk <laniusone@pm.me>
2025-02-11 16:08:59 +0100

    WWATCH-17 - Magento Staging module compatibility

--- Plugin/ParentIds.php
+++ Plugin/ParentIds.php
@@ -1,21 +1,20 @@
 <?php
 /**
- * Created by Q-Solutions Studio
- * Date: 01.07.19
+ * Created by Qoliber
  *
  * @category    DataFeedWatch
  * @package     DataFeedWatch_Connector
  * @author      Maciej Buchert <maciej@qsolutionsstudio.com>
+ * @author      Wojciech M. Wnuk <wwnuk@qoliber.com>
  */
 
 namespace DataFeedWatch\Connector\Plugin;
 
 use Magento\Catalog\Api\Data\ProductExtensionFactory;
-use Magento\Catalog\Model\ProductRepository;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Type;
-use Magento\Framework\Api\SearchResults;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Module\Manager;
 
 /**
  * Class ParentIds
@@ -23,7 +22,28 @@ use Magento\Framework\App\ResourceConnection;
  */
 class ParentIds extends ExtensionAttributeAbstract
 {
-    const RELATIONS_TABLE = "catalog_product_relation";
+    public const ENTITY_TABLE = 'catalog_product_entity';
+    public const RELATIONS_TABLE = 'catalog_product_relation';
+
+    /**
+     * @var \Magento\Framework\Module\Manager
+     */
+    protected $moduleManager;
+
+    /**
+     * @param  \Magento\Catalog\Api\Data\ProductExtensionFactory $extensionFactory
+     * @param  \Magento\Framework\App\ResourceConnection $resourceConnection
+     * @param  \Magento\Framework\Module\Manager         $moduleManager
+     */
+    public function __construct(
+        ProductExtensionFactory $extensionFactory,
+        ResourceConnection $resourceConnection,
+        Manager $moduleManager
+    ) {
+        parent::__construct($extensionFactory, $resourceConnection);
+
+        $this->moduleManager = $moduleManager;
+    }
 
     /**
      * @param Product $product
@@ -45,14 +65,28 @@ class ParentIds extends ExtensionAttributeAbstract
     {
         $connection = $this->resourceConnection->getConnection();
         $tableName = $this->resourceConnection->getTableName(self::RELATIONS_TABLE);
+        $entityTable = $this->resourceConnection->getTableName(self::ENTITY_TABLE);
 
-        $query = $this->resourceConnection
-            ->getConnection()
-            ->select()
-            ->from($tableName, 'parent_id')
-            ->where(sprintf('child_id = %s', $product->getId()));
+        if (!$this->moduleManager->isEnabled('Magento_Staging')) {
+            $query = $this->resourceConnection
+                ->getConnection()
+                ->select()
+                ->from($tableName, 'parent_id')
+                ->where(sprintf('child_id = %s', $product->getId()));
 
-        return $connection->fetchCol($query);
+            return $connection->fetchCol($query);
+        } else {
+            $query = $connection->select()
+                ->from(['r' => $tableName], 'parent_id')
+                ->join(
+                    ['e' => $entityTable],
+                    'r.parent_id = e.row_id',
+                    ['parent_id' => 'e.entity_id']
+                )
+                ->where('r.child_id = ?', $product->getId());
+
+            return $connection->fetchCol($query);
+        }
     }
 
     /**
